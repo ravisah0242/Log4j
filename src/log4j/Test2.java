f6c385cf2f96846164673b9be654a23a6a73f48a
Ravi Ranjan Kumar <70317014+ravisah0242@users.noreply.github.com>
2020-11-20 01:18:03 +0530

    Update Test2.java

--- src/log4j/Test2.java
+++ src/log4j/Test2.java
@@ -6,6 +6,11 @@ public class Test2
 	public static void main(String[] args)
 	{
 		System.out.println("Pari");
+		System.out.println("kk");
+		System.out.println("ll");
+		System.out.println("kk");
+		System.out.println("Pbbari");
+		System.out.println("bb");
 
 	}
 
